1910b5a61b0237d701449865c0294824e6996250
Chandanaschandu <chandanaschandanas491@gmail.com>
2024-12-05 20:10:27 +0530

    Code for Reverse slice add Code for Reverse an slice

---
+++ sliceToMap/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func SliceToMap(slice []int) map[int]int {
+
+	result := make(map[int]int)
+
+	for i, value := range slice {
+		result[value] = i
+	}
+
+	return result
+}
+
+func main() {
+
+	s := []int{1, 2, 3, 4}
+
+	result := SliceToMap(s)
+
+	fmt.Println(result)
+}
