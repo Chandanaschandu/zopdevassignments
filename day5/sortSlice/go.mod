7bd3f73ffe34e874b6d81598dfbcbbb7897b8508
Chandanaschandu <chandanaschandanas491@gmail.com>
2024-12-11 14:43:49 +0530

    removed .idea file

---
+++ day5/sortSlice/go.mod
@@ -0,0 +1,3 @@
+module github.com/Chandanaschandu/sortSlice
+
+go 1.23.3
--- day5/sortSlice/main.go
+++ day5/sortSlice/main.go
@@ -1,1 +1,29 @@
-package sortSlice
+package main
+
+import "fmt"
+
+func sortSlice(ar []int, compare func(int, int) bool) {
+	for i := 0; i < len(ar)-1; i++ {
+
+		for j := i + 1; j < len(ar); j++ {
+			if compare(ar[i], ar[j]) {
+				ar[j], ar[i] = ar[i], ar[j]
+			}
+		}
+
+	}
+}
+
+func compare(a, b int) bool {
+	if a > b {
+		return true
+	} else {
+		return false
+	}
+}
+func main() {
+
+	ar := []int{2, 1, 9, 3, 5}
+	sortSlice(ar, compare)
+	fmt.Println(ar)
+}
