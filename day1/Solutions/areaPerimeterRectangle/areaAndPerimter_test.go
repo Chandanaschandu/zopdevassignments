7bf9cc419d699fedc3d8565a08f8fd7e7a835a9a
Chandanaschandu <chandanaschandanas491@gmail.com>
2024-12-11 22:04:15 +0530

    test case

--- day1/Solutions/areaPerimeterRectangle/areaAndPerimter_test.go
+++ day1/Solutions/areaPerimeterRectangle/areaAndPerimter_test.go
@@ -14,6 +14,15 @@ func TestAreaRectangle(t *testing.T) {
 		{1, 5, 5.0},
 	}
 
+	for _, test := range tests {
+		res := AreaRectangle(test.l, test.b)
+		if res != test.output {
+			t.Errorf("The length %f  and breadth %f  area is %f:", test.l, test.b, test.output)
+		}
+	}
+
+}
+func TestPeriRectangle(t *testing.T) {
 	tests1 := []struct {
 		a       float64
 		b       float64
@@ -22,19 +31,13 @@ func TestAreaRectangle(t *testing.T) {
 		{2, 3, 10},
 		{3, 4, 14},
 	}
-
-	for _, test := range tests {
-		res := AreaRectangle(test.l, test.b)
-		if res != test.output {
-			t.Errorf("The length %f  and breadth %f  area is %f:", test.l, test.b, test.output)
-		}
-	}
 	for _, v := range tests1 {
 		res1 := PeriRectangle(v.a, v.b)
 		if res1 != v.output1 {
 			t.Errorf("For length %f and breadth %f perimeter of rectangle is :%f", v.a, v.b, v.output1)
 		}
 	}
+
 }
 func BenchmarkAreaRectangle(b *testing.B) {
 	tests := []struct {
@@ -46,6 +49,14 @@ func BenchmarkAreaRectangle(b *testing.B) {
 		{1, 5, 5.0},
 	}
 
+	for range b.N {
+		for _, test := range tests {
+			AreaRectangle(test.l, test.b)
+		}
+	}
+
+}
+func BenchmarkPeriRectangle(b *testing.B) {
 	tests1 := []struct {
 		a       float64
 		b       float64
@@ -53,11 +64,6 @@ func BenchmarkAreaRectangle(b *testing.B) {
 	}{
 		{2, 3, 10},
 		{3, 4, 14},
-	}
-	for range b.N {
-		for _, test := range tests {
-			AreaRectangle(test.l, test.b)
-		}
 	}
 	for range b.N {
 		for _, test := range tests1 {
