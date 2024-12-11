d7b71a4f2b08de1fe9292b9dc863e72a08cd0b01
Chandanaschandu <chandanaschandanas491@gmail.com>
2024-12-11 14:11:26 +0530

    add employee details code

--- day5/employeeName/employeeFirstName.go
+++ day5/employeeName/employeeFirstName.go
@@ -1,1 +1,14 @@
 package employeeName
+
+import (
+	"fmt"
+)
+
+type Employee struct {
+	FirstName string
+	LastName  string
+}
+
+func GreetEmployee(e Employee) string {
+	return fmt.Sprintf("Hello %s %s", e.FirstName, e.LastName)
+}
--- day5/employeeName/employee_test.go
+++ day5/employeeName/employee_test.go
@@ -1,1 +1,28 @@
 package employeeName
+
+import "testing"
+
+func TestGreetEmployee(t *testing.T) {
+	employee := Employee{
+		FirstName: "Chandana",
+		LastName:  "S",
+	}
+
+	expected := "Hello Chandana S"
+	result := GreetEmployee(employee)
+	if result != expected {
+		t.Errorf("GreetEmployee(%v) = %q; want %q", employee, result, expected)
+	}
+
+}
+func BenchmarkGreetEmployee(b *testing.B) {
+	employee := Employee{
+		FirstName: "Chandana",
+		LastName:  "S",
+	}
+
+	//expected := "Hello Chandana S"
+	for range b.N {
+		GreetEmployee(employee)
+	}
+}
